7a9515494f341fcb0099087fdfc306da6610220b
Aleksandra <jofleytowaffles@gmail.com>
2022-06-27 17:02:00 +0500

    Переверстана страница входа

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
-import M from 'materialize-css';
-import { UserService } from '../services/user.service';
-import { AssetService } from '../services/asset.service';
-import { interval } from 'rxjs';
-import { DatabaseService } from '../services/database.service';
-import { ErrorService } from '../services/error.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
+import M from "materialize-css";
+import { UserService } from "../services/user.service";
+import { AssetService } from "../services/asset.service";
+import { interval } from "rxjs";
+import { DatabaseService } from "../services/database.service";
+import { ErrorService } from "../services/error.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
 export class AuthComponent implements OnInit {
-  public error = '';
+  public isPasswordVisible: boolean = false;
+  public error = "";
+
   constructor(
     private auth: AuthService,
     private router: Router,
@@ -25,8 +27,13 @@ export class AuthComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
   login(login: string, password: string) {
-    this.error = '';
+    this.error = "";
     this.auth
       .login(login, password)
       .then(
@@ -36,33 +43,33 @@ export class AuthComponent implements OnInit {
           id: string;
           token: string;
         }) => {
-          this.asset.setToStorage('token', res.token).then();
-          this.asset.setToStorage('login', login).then();
-          this.asset.setToStorage('last_password', password).then();
+          this.asset.setToStorage("token", res.token).then();
+          this.asset.setToStorage("login", login).then();
+          this.asset.setToStorage("last_password", password).then();
           this.user.token = res.token;
           this.user.login = login;
           this.user.last_password = password;
           this.db
             .signup(login, password)
             .then((res) => {
-              console.log(res, 'Log In res');
+              console.log(res, "Log In res");
             })
             .catch((err) => {
               if (err === 400) {
                 this.db
                   .logIN(login, password)
                   .then((res) => {
-                    console.log(res, 'Log In res');
+                    console.log(res, "Log In res");
                   })
                   .catch((err) => {
-                    console.log(err, 'Log In err');
+                    console.log(err, "Log In err");
                   });
               }
             });
-          this.router.navigateByUrl('devices').then(() => {
-            if (res.error === 'change super admin password') {
+          this.router.navigateByUrl("devices").then(() => {
+            if (res.error === "change super admin password") {
               let i = interval(1000).subscribe(() => {
-                let elem = document.querySelector('.modal');
+                let elem = document.querySelector(".modal");
                 const options = {
                   dismissible: false,
                 };
