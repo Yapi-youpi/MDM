c94f69270c4321799cd5899c1248ae1d0fe57679
Aleksandra <jofleytowaffles@gmail.com>
2022-07-18 11:54:42 +0500

    Инпут поиска

--- src/app/components/common/UI/input/search-input/search-input.component.ts
+++ src/app/components/common/UI/input/search-input/search-input.component.ts
@@ -3,16 +3,25 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 @Component({
   selector: "app-search-input",
   templateUrl: "./search-input.component.html",
-  styleUrls: ["./search-input.component.css"],
+  styleUrls: ["./search-input.component.scss"],
 })
 export class SearchInputComponent {
   @Input() public value: string = "";
 
   @Output() onChange = new EventEmitter<string>();
 
+  public isSearchFocused: boolean = false;
+
   constructor() {}
 
   onChangeHandler(value: string) {
     this.onChange.emit(value);
+  }
+
+  setSearchFocus() {
+    this.isSearchFocused = true;
+  }
+  resetSearchFocus() {
+    this.isSearchFocused = false;
   }
 }
