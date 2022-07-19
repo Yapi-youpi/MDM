720ed28debdef242e8d2e0456f28450c75d9f867
Anna <anna.zhukanna@gmail.com>
2022-07-19 12:14:08 +0500

    Добавлена базовая верстка страницы редактирвоания конфигураций, настроен роутинг

--- src/app/components/pages/configuration/configuration.component.ts
+++ src/app/components/pages/configuration/configuration.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DevicesConfigService } from '../../../services/devices-config.service';
+import { DevicesConfig } from '../../../interfaces/interfaces';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
-  styleUrls: ['./configuration.component.css']
+  styleUrls: ['./configuration.component.css'],
 })
 export class ConfigurationComponent implements OnInit {
+  public config!: DevicesConfig;
+  constructor(
+    private route: ActivatedRoute,
+    private configService: DevicesConfigService,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getConfig();
   }
 
+  getConfig() {
+    const id = this.route.snapshot.paramMap.get('id') || 'default';
+    console.log(id);
+    this.configService
+      .getConfig(id)
+      .then((res) => {
+        this.config = Object.assign(res[0]);
+        console.log(this.config);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  goBack() {
+    this.router.navigateByUrl('config');
+  }
 }
