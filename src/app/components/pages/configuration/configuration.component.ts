7a3fa5439278837b56585750bdd1616da08263f2
Anna <anna.zhukanna@gmail.com>
2022-07-20 17:35:55 +0500

    Форма редактирования конфигураций разделена на табы

--- src/app/components/pages/configuration/configuration.component.ts
+++ src/app/components/pages/configuration/configuration.component.ts
@@ -89,4 +89,35 @@ export class ConfigurationComponent implements OnInit {
   goBack() {
     this.router.navigateByUrl('config');
   }
+
+  setActive(event) {
+    const target = event.target;
+    const tabs = document.querySelectorAll('.tab');
+    const tabsContent = document.querySelectorAll('.form__tab');
+
+    if (target && target.matches('.tab')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+    function hideTabContent() {
+      tabsContent.forEach((item) => {
+        // @ts-ignore
+        item.style.display = 'none';
+      });
+      tabs.forEach((item) => {
+        item.classList.remove('tab--active');
+      });
+    }
+
+    function showTabContent(i = 0) {
+      console.log(tabsContent[i]);
+      // @ts-ignore
+      tabsContent[i].style.display = 'flex';
+      tabs[i].classList.add('tab--active');
+    }
+  }
 }
