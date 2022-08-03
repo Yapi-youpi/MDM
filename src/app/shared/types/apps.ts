6e8ea3988ce81936b2314b44f43395b9e44453b2
Aleksandra <jofleytowaffles@gmail.com>
2022-08-03 16:16:02 +0500

    Интерфейс групп приложений

--- src/app/shared/types/apps.ts
+++ src/app/shared/types/apps.ts
@@ -25,3 +25,32 @@ export interface App {
   screenOrder: number;
   bottom: boolean;
 }
+
+export interface AppGroup {
+  ID: string;
+  children: App[];
+
+  configID: string;
+  fileName: string;
+  localPath: string;
+  pkg: string;
+  name: string;
+  url: string;
+  version: string;
+  arch: string;
+  latestVersion: string;
+  iconText: string;
+  iconPath: string;
+  iconUID: string;
+  fileSize: string;
+  fileByteSize: number;
+  showIcon: boolean;
+  availableInStorage: boolean;
+  useKiosk: boolean;
+  system: boolean;
+  runAfterInstall: boolean;
+  runAtBoot: boolean;
+  skipVersion: boolean;
+  screenOrder: number;
+  bottom: boolean;
+}
