34eb0a65d393b98bc264aacffc15baed7c514dd8
Anna <anna.zhukanna@gmail.com>
2022-08-05 10:05:38 +0500

    Исправлено выделение активных пунктов меню при переходе по страницам

--- src/app/components/common/menu/menu.component.ts
+++ src/app/components/common/menu/menu.component.ts
@@ -1,23 +1,23 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
-  selector: "app-menu",
-  templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.scss"],
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
   @Input() public isSidebarHidden!: boolean;
-
   @Output() onSidebarHide = new EventEmitter<any>();
-
   constructor() {}
 
-  ngOnInit(): void {
-    let path = window.location.pathname.slice(1);
-    let elem = document.getElementById(`${path}`);
-    if (elem) {
-      elem.classList.add("active");
-    }
+  ngOnInit() {
+    let t = timer(20).subscribe(() => {
+      const path = window.location.pathname.slice(1);
+      const elem = document.getElementById(path);
+      elem?.classList.add('active');
+      t.unsubscribe();
+    });
   }
 
   toggleSidebarView() {
