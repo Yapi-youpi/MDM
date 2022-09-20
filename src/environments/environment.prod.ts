06a814f36d55d8108fb0804ace0054f92f7546cb
Aleksandra <jofleytowaffles@gmail.com>
2022-09-20 12:49:18 +0500

    Релокейшн конфигов парса для проды

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,13 @@ export const environment = {
   production: true,
   // url: 'http://172.24.18.25:2525',
   url: 'http://62.113.111.186:2525',
+  parseInit: {
+    appId: 'MDMSERVER12324990543BSFD93234',
+    jsKey: 'U52RUG55222VAJ5478JDE32T3TE223B77YA',
+    serverURL: 'http://45.147.176.126:1337/parse',
+    lqServerURL: 'ws://45.147.176.126:8189',
+  },
+  parseClasses: {
+    devices: 'Device',
+  },
 };
